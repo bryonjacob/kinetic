d88196ec5092eebea538ac969f6457d9c4a1d940
bjacob <bjacob@homeaway.com>
2015-07-03 23:01:05 -0500

    added kinesis

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var kinesis = require('kinesis');
 
 app.get('/', function(req, res) {
     res.send('Hello, Node');
+});
+
+sink = kinesis.stream('kinetic');
+
+app.get('/send', function(req, res) {
+   sink.write('test message');    
 });
 
 app.listen(80);
